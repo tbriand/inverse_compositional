8231265d88046f9061e1039c57458e3d80d3c2c4
Thibaud BRIAND <thibaud.briand@enpc.fr>
2017-10-19 14:12:56 +0200

    Full new latex improved

--- experiments/evaluation_modification_improved_latex.sh
+++ experiments/evaluation_modification_improved_latex.sh
@@ -44,9 +44,6 @@ SAVE=1          #saving with good precision is necessary
     echo "\\end{center}"
     echo "\\end{table}"
 
-#                             basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
-#                             time_ica=time_ica_${basefile}.txt
-#                             rmse_ica=rmse_ica_${basefile}.txt
 # ICA COLOR
     echo ""
     FIRST_SCALE=0
@@ -86,16 +83,218 @@ SAVE=1          #saving with good precision is necessary
                         line2echo="$line2echo & `cat $time_ica`"
                     done
             done
+            if [ ! "$noise" -eq "50" ]; do 
+                line2echo="$line2echo \\\\"
+            done
+            echo "$line2echo"
+            done
+            cd ..
+    done
+    echo "\\end{tabular}"
+    echo "\\caption{Influence of the color handling}"
+    echo "\\end{center}"
+    echo "\\end{table}"
+    
+# ICA DBP
+    echo ""
+    FIRST_SCALE=0
+    ROBUST_GRADIENT=0
+    GRAYMETHOD=1
+    echo "\\begin{table}"
+    echo "\\begin{center}"
+    echo "\\begin{tabular}{l|l|l|l|l|l|l|l|l|l|l|l}"
+    echo "\\multicolumn{2}{l}{} &\\multicolumn{2}{|c|}{L2} & \\multicolumn{2}{c|}{Truncated L2} & \\multicolumn{2}{c|}{German} & \\multicolumn{2}{c|}{Lorentzian} & \\multicolumn{2}{c}{Charbonnier} \\\\"
+    echo "\\hline"
+    echo "\\multicolumn{2}{l|}{} & &  DBP & & DBP & & DBP & & DBP & & DBP\\\\"
+    for noise in 0 3 5 10 20 30 50; do
+            dir=noise$noise
+            cd $dir
+            echo "\\hline"
+            
+            #RMSE
+            line2echo="\\multirow{2}{0.1\\linewidth}{$\sigma = $noise$} & RMSE"
+            for ROBUST in 0 1 2 3 4; do
+                    for EDGEPADDING in 0 5; do
+                        basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
+                        # field comparison
+                        rmse_ica=rmse_ica_${basefile}.txt
+                        line2echo="$line2echo & `mean_and_std $rmse_ica 0`"
+                    done
+            done
             line2echo="$line2echo \\\\"
+            echo "$line2echo"
+
+            #TIME
+            line2echo=" & Time"
+            for ROBUST in 0 1 2 3 4; do
+                    for EDGEPADDING in 0 5; do
+                        basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
+                        # field comparison
+                        time_ica=time_ica_${basefile}.txt
+                        line2echo="$line2echo & `cat $time_ica`"
+                    done
+            done
+            if [ ! "$noise" -eq "50" ]; do 
+                line2echo="$line2echo \\\\"
+            done
             echo "$line2echo"
             done
 
-            echo "\\end{tabular}"
-            echo "\\caption{Influence of the color handling}"
-            echo "\\end{center}"
-            echo "\\end{table}"
+            cd ..
+    done
+    echo "\\end{tabular}"
+    echo "\\caption{Influence of discarding boundary pixels (using the gray method)}"
+    echo "\\end{center}"
+    echo "\\end{table}"
+
+# ICA GRADIENT
+    echo ""
+    FIRST_SCALE=0
+    ROBUST=4
+    GRAYMETHOD=1
+    EDGEPADDING=5
+    echo "\\begin{table}"
+    echo "\\begin{center}"
+    echo "\\begin{tabular}{l|l|l|l|l|l|l|l}"
+    echo "\\multicolumn{2}{l}{} & Central Differences & Hypomode & Farid 3x3 & Farid 5x5 & Gaussian 3 & Gaussian 6 \\\\"
+    for noise in 0 3 5 10 20 30 50; do
+            dir=noise$noise
+            cd $dir
+            echo "\\hline"
+            
+            #RMSE
+            line2echo="\\multirow{2}{0.1\\linewidth}{$ \sigma = $noise $} & RMSE"
+            for ROBUST_GRADIENT in 0 1 2 3 4 5; do
+                basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
+                # field comparison
+                rmse_ica=rmse_ica_${basefile}.txt
+                line2echo="$line2echo & `mean_and_std $rmse_ica 0`"
+            done
+            line2echo="$line2echo \\\\"
+            echo "$line2echo"
+
+            #TIME
+            line2echo=" & Time"
+            for ROBUST_GRADIENT in 0 1 2 3 4 5; do
+                basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
+                # field comparison
+                time_ica=time_ica_${basefile}.txt
+                line2echo="$line2echo & `cat $time_ica`"
+            done
+            if [ ! "$noise" -eq "50" ]; do 
+                line2echo="$line2echo \\\\"
+            done
+            echo "$line2echo"
+            done
+            
+            cd ..
+    done
+    echo "\\end{tabular}"
+    echo "\\caption{Influence of the gradient estimator (using the Charbonnier robust function, the gray method and discarding boundary pixels)}"
+    echo "\\end{center}"
+    echo "\\end{table}"
+
+    
+# ICA FIRST SCALE    
+    echo ""
+    ROBUST_GRADIENT=3
+    ROBUST=4
+    GRAYMETHOD=1
+    EDGEPADDING=5
+    echo "\\begin{table}"
+    echo "\\begin{center}"
+    echo "\\begin{tabular}{l|l|l|l|l|l}"
+    echo "\\multicolumn{2}{l}{} & $ s_0 = 0 $ & $ s_0 = 1 $ & $ s_0 = 2 $ & $ s_0 = 3 $ \\\\"
+    for noise in 0 3 5 10 20 30 50; do
+            dir=noise$noise
+            cd $dir
+            echo "\\hline"
+            
+            #RMSE
+            line2echo="\\multirow{2}{0.1\\linewidth}{$\sigma = $noise$} & RMSE"
+            for FIRST_SCALE in 0 1 2 3; do
+                basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
+                # field comparison
+                rmse_ica=rmse_ica_${basefile}.txt
+                line2echo="$line2echo & `mean_and_std $rmse_ica 0`"
+            done
+            line2echo="$line2echo \\\\"
+            echo "$line2echo"
+
+            #TIME
+            line2echo=" & Time"
+            for FIRST_SCALE in 0 1 2 3; do
+                basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
+                # field comparison
+                time_ica=time_ica_${basefile}.txt
+                line2echo="$line2echo & `cat $time_ica`"
+            done
+            if [ ! "$noise" -eq "50" ]; do 
+                line2echo="$line2echo \\\\"
+            done
+            echo "$line2echo"
+            done
 
             cd ..
     done
+    echo "\\end{tabular}"
+    echo "\\caption{Influence of the first scale (using the Charbonnier robust function, the gray method, the Farid 5x5 kernel estimator and discarding boundary pixels)}"
+    echo "\\end{center}"
+    echo "\\end{table}"
+
+# COMPARISON ALL METHODS    
+    echo ""
+    ROBUST=4
+    GRAYMETHOD=0
+    FIRST_SCALE=0
+    EDGEPADDING=0
+    ROBUST_GRADIENT=0
+    echo "\\begin{table}"
+    echo "\\begin{center}"
+    echo "\\begin{tabular}{l|l|l|l|l|l}"
+    echo "\\multicolumn{2}{l}{}  $ SIFT + RANSAC  &  IC & IC optimized $ \\\\"
+    for noise in 0 3 5 10 20 30 50; do
+            dir=noise$noise
+            cd $dir
+            echo "\\hline"
+            
+            basefile=graymethod${GRAYMETHOD}_save${SAVE}_scale${FIRST_SCALE}_edge${EDGEPADDING}_gradient${ROBUST_GRADIENT}_robust${ROBUST}
+            
+            #RMSE
+            line2echo="\\multirow{2}{0.1\\linewidth}{$\sigma = $noise$} & RMSE"
+            #sift
+            line2echo="$line2echo & `mean_and_std $rmse_sift 0`"
+
+            #ic
+            rmse_ica=rmse_ica_${basefile}.txt
+            line2echo="$line2echo & `mean_and_std $rmse_ica 0`"
+
+            #ic optimized
+            line2echo="$line2echo & ?"
+            line2echo="$line2echo \\\\"
+            echo "$line2echo"
+
+            #TIME
+            line2echo=" & Time"
+            #sift
+            line2echo="$line2echo & `cat $time_sift`"
+
+            #ic
+            time_ica=time_ica_${basefile}.txt
+            line2echo="$line2echo & `cat $time_ica`"
+
+            #ic optimized
+            line2echo="$line2echo & ?"
+            if [ ! "$noise" -eq "50" ]; do 
+                line2echo="$line2echo \\\\"
+            done
+            echo "$line2echo"
+
+            cd ..
+    done
+    echo "\\end{tabular}"
+    echo "\\caption{Comparison of the methods. The two IC variants use the Charbonnier robust function. The optimized IC algorithm uses the gray method, the Farid 5x5 kernel estimator and discards boundary pixels. The first scale used is precised in parenthesis.}"
+    echo "\\end{center}"
+    echo "\\end{table}"
 
 cd ..
