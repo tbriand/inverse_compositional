4e72b81f43a120fce1ff1559ba643f89e1aebf33
Thibaud BRIAND <thibaud.briand@enpc.fr>
2017-12-15 16:27:02 +0100

    Correct experiments all image

--- experiments/evaluation_modification_all_images.sh
+++ experiments/evaluation_modification_all_images.sh
@@ -4,5 +4,6 @@ L=20
 for image in affinity2 homography22 homography2 identity2 rotation_translation2 translation2 zoom2 zoom_rotation2 zoom_rotation_translation2 lena; do
     im=$dirimage/$image.png
     ./evaluation_modification.sh $im $number $L $image
-    ./evaluation_modification_latex.sh $image
+    ./evaluation_modification_latex.sh $image > ${image}_L20_avocat.txt
+    rm -rf $image
 done
