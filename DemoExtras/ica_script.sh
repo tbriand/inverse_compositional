7a3cb9867d96996d9feac1ab5cffe48630d2c787
Thibaud Briand (Dell notebook) <briand.thibaud@gmail.com>
2018-01-12 18:02:23 +0100

    Correct demo ipol script

--- DemoExtras/ica_script.sh
+++ DemoExtras/ica_script.sh
@@ -36,6 +36,7 @@ ref_noisy=input_noisy_0.png
 warped=input_1.png
 warped_noisy=input_noisy_1.png
 file=transformation.txt
+filewithout=transformation_without.txt
 if [ -f input_2.txt ]; then
     file2=input_2.txt
 else
@@ -45,7 +46,20 @@ fi
 echo "Standard deviation of the noise added: $std"
 add_noise $ref $ref_noisy $std
 add_noise $warped $warped_noisy $std
+
 echo ""
+GRAYMETHOD=0 EDGEPADDING=0 NANIFOUTSIDE=0 ROBUST_GRADIENT=0 inverse_compositional_algorithm $ref_noisy $warped_noisy -f $filewithout -z $zoom -n $nscales -r $robust -e $eps -t $transform -s 0 > /dev/null
 GRAYMETHOD=$GRAYMETHOD EDGEPADDING=$edgepadding NANIFOUTSIDE=$NANIFOUTSIDE ROBUST_GRADIENT=$gradient inverse_compositional_algorithm $ref_noisy $warped_noisy -f $file -z $zoom -n $nscales -r $robust -e $eps -t $transform -s $first_scale -v
+
 echo ""
+echo "Without modification:"
+NANIFOUTSIDE=1 EDGEPADDING=0 generate_output $ref_noisy $warped_noisy $filewithout $file2
+mv output_estimated.png output_estimated2.png
+if [ -f epe.png ]; then
+    mv epe.png epe2.png
+fi
+mv diff_image.png diff_image2.png
+
+echo ""
+echo "With modifications:"
 NANIFOUTSIDE=1 EDGEPADDING=0 generate_output $ref_noisy $warped_noisy $file $file2
